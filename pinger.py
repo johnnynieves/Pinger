6ea6c0645bcf7fbac6aa461fbd13568e461803a8
johnnynieves <johnny.nieves@live.com>
2021-03-17 17:23:40 -0700

    Working on testing alert function

--- pinger.py
+++ pinger.py
@@ -7,48 +7,58 @@ from colorama import Fore, Style
 from os import error, system
 
 
-def mport():
-    # do a check on the system to see if modules already exist if they do move on to start()
-    system('sudo -H pip3 install icmplib colorama')
+def newscreen():
+    system('clear')
 
 
 def start():
     for year in range(0, 365 + 1):
-        with open('Standard_Log.csv', 'a+') as log:
+        with open(f'Standard_Log_{ctime()}.csv', 'a+') as log:
             print(f'Starting log for {ctime()}\n')
-            log.write(f'Starting log for {ctime()}\n')
+            log.write(f'Monitoring Log for {ctime()}\n')
             log.write('')
             log.write(f'DEVICE     ,NETWORK IP     ,STATUS     ,LOG_TIME\n')
-        for minutes in range(0, 1440):
-            ping_devices()
-            sleep(60)
-            system('clear')
+            for minutes in range(0, 1440):
+                ping_devices(log)
+                sleep(5)
+                system('clear')
 
 
-def ping_devices():
+def ping_devices(log):
+    # check1 = 'blank'
+    # check2 = 'blank2'
     with open('devices.csv', 'r') as device:
         devices = reader(device)
-        with open('Standard_Log.csv', 'a+') as log:
-            for object in devices:
-                response = ping(object[1], count=1)
-                if response.is_alive:
-                    entre = f'{object[0]} {object[1]} | is{Fore.GREEN} Up{Style.RESET_ALL} | Log {ctime()}'
-                    print(entre)
-                    log.write(
-                        f'{object[0]},{object[1]},Up,{ctime()}\n')
-                else:
-                    dead = f'{object[0]} {object[1]} | is{Fore.LIGHTRED_EX} Down{Style.RESET_ALL} | Log {ctime()}'
-                    print(dead)
-                    log.write(
-                        f'{object[0]},{object[1]},Down,{ctime()}\n')
-                    # add a counter: if down twice alert people
+        for object in devices:
+            response = ping(object[1], count=1)
+            if response.is_alive:
+                entre = f'{object[0]} {object[1]} | is{Fore.GREEN} Up{Style.RESET_ALL} | Log {ctime()}'
+                print(entre)
+                log.write(
+                    f'{object[0]},{object[1]},Up,{ctime()}\n')
+            else:
+                dead = f'{object[0]} {object[1]} | is{Fore.LIGHTRED_EX} Down{Style.RESET_ALL} | Log {ctime()}'
+                print(dead)
+                log.write(f'{object[0]},{object[1]},Down,{ctime()}\n')
+            # alert(check1, object[1])
+                # check2 = object[1]
+                # if check1 == check2:
+                #     print(
+                #         f"This is your alert {check2} has been {Fore.LIGHTRED_EX} Down{Style.RESET_ALL} for atleast 2 minutes!!")
+                # check2 = check1
+
+
+def alert(check1, check2):
+    if check1 == check2:
+        print(
+            f"This is your alert {check2} has been {Fore.LIGHTRED_EX} Down{Style.RESET_ALL} for aleast 2 minutes!!")
 
 
 if '__main__' == __name__:
-    try:
-        mport()
-        start()
-    except:
-        KeyboardInterrupt
-        print('\nYou decided to quit the Pinger?!\nGood Bye!!')
-        exit(1)
+    # try:
+    newscreen()
+    start()
+    # except:
+    #     KeyboardInterrupt
+    #     print('\nYou decided to quit the Pinger?!\nGood Bye!!')
+    #     exit(1)
