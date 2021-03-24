5a8eb8f36e3c9a41581db6000c929defa90ddb41
Johnny Nieves <50960412+johnnynieves@users.noreply.github.com>
2021-03-24 08:26:11 -0700

    Update pinger.ps1
    Condensed coded | Fixed logging Issue | Created alerts

--- pinger.ps1
+++ pinger.ps1
@@ -1,36 +1,32 @@
 
+#Variables
 $year = 365 + 1
-
-foreach ($day in $year) {
-    # New-item -ItemType "file" -Path .\Standard_Log $(Get-Date).csv -Header "DEVICE", "NETWORK_IP", "STATUS", "LOG_TIME"
-    Write-Host 'Starting log for' $(Get-Date)
-    Write-Host '*************************************************************'
-
-}
-
-
-# for year in range(0, 365 + 1):
-#   with open(f'Standard_Log_ { ctime() }.csv', 'a+') as log:
-# print(f'Starting log for { ctime() }\n')
-# log.write(f'Monitoring Log for { ctime() }\n')
-# log.write('')
-# log.write(f'DEVICE     , NETWORK IP     , STATUS     , LOG_TIME\n')
-# for minutes in range(0, 1440):
-# ping_devices(log)
-# sleep(5)
-# system('clear')
-
-
-
+$day = 1440 +1
 $devices = Import-Csv -path .\devices.csv -Header "Host", "IP" -delimiter ","
-Foreach ($device in $devices) {
-    $pingRequest = Test-Connection $device.IP -Count 1 -Quiet
-    if ($PingRequest -eq $false) {
-        Write-Host "Status for $device is DOWN | Log $(Get-Date)" 
-        Write-Output @($device.Host, $device.IP, "DOWN", $(Get-Date)) | Out-file -Append -filepath ./Standard_Log.csv
-    }
-    else {
-        Write-Host "Status for $device is UP | Log $(Get-Date)"
-        Write-Output @($device.Host, $device.IP, "UP", $(Get-Date)) | Out-File -Append -filepath ./Standard_Log.csv 
+
+#Start Basic WhatsupGold
+
+For($day=0;$day -le $year; $day++) {
+    clear
+    Write-Host "Starting log for" $(Get-Date -f yyyy/MM/dd)
+    Write-Host "*************************************************************"
+    Set-Content .\Standard_Log.csv "DEVICE & IP,STATUS, LOG_TIME"
+    For($minute=0;$minute -le $day; $day++){
+        Foreach ($device in $devices) {
+            $PingRequest = Test-Connection $device.IP -Count 1 -Quiet
+            if ($PingRequest -eq $false) {
+                Write-Host "Status for $device is  DOWN | Log $(Get-Date)" -ForegroundColor Red
+                $list = "$device,DOWN,$(Get-Date)"
+                Write-Output $list | Out-file -Append -filepath .\Standard_Log.csv
+                msg console /server:localhost $list
+            }
+            else {
+                Write-Host "Status for $device is UP | Log $(Get-Date)" -ForegroundColor Green
+                $list = "$device,UP,$(Get-Date)"
+                Write-Output $list | Out-File -Append -filepath .\Standard_Log.csv
+            }
+        }
+        Start-Sleep -Seconds 300  #Every 5 Minutes
     }
 }
+
